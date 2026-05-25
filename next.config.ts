bb6552a6610e53cae4a90278318ad43d54cbae46
youtrylistenx-gif <youtrylistenx@gmail.com>
2026-05-25 16:27:19 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/my-planner-app",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
